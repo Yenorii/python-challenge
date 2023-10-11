25f156a70d03ac6917db96d034dfdad0d86b4533
yenorii <144069387+Yenorii@users.noreply.github.com>
2023-10-10 23:12:50 -0400

    homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,14 +5,18 @@ import csv
 # set the path for the csv
 budget_data_csv = os.path.join("/Users/yenori/Desktop/GitHub/python-challenge/PyBank/Resources")
 
-# set the variables
+# (?) set the variables (?)
 total_months = 0
-total_revenue = 0
-revenue_of_change = 0
-revenue_average = 0
 
 # open the csv
 with open ('budget_data.csv') as csvfile:
     csvreader = csv.dictReader(csvfile)
 
+
+# TASK 1: Calculate the total number of months included in the dataset
+    for row in csvreader:
+        total_months += 1
+
+
+    
 

9afd5d85e18c02e6ec2d2cfef1f29479fb7395a5
yenorii <144069387+Yenorii@users.noreply.github.com>
2023-10-26 19:29:48 -0400

    homework

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,15 +2,13 @@
 import os
 import csv
 
-# set the path for the csv
+# set path and open csv
 election_data_csv = os.path.join("/Users/yenori/Desktop/GitHub/python-challenge/PyPoll/Resources")
-
-# set the variables
-
-
-# open the csv
 with open ('election_data.csv') as csvfile:
     csvreader = csv.dictReader(csvfile)
+
+# set variables
+
 
 # Calculate the total number of votes cast:
 

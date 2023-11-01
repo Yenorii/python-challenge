1212cc307b450c61c4214aab96ded0b127bb198c
yenorii <144069387+Yenorii@users.noreply.github.com>
2023-10-31 21:15:58 -0400

    homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,14 @@ import os
 import csv
 
 # set path, open file, and loop through columns
-budget_data_csv = os.path.join("/Users/yenori/Desktop/GitHub/python-challenge/PyBank/Resources")
-
-# variables:
+budget_data_csv = os.path.join("Resources/budget_data.csv")
+with open (budget_data_csv, 'r') as budgetcsv:
+    csv_reader = csv.reader(budgetcsv)
+    header = next (csv_reader)
+    print (header[0])
+    for row in csv_reader:
+        counter += int(row [1])
+        Date = row [0]
+        print (Date)
+        print (counter)
+print (counter)
